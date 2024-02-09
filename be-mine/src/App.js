53177c6175aed002c38f60c2ffcb887e8227b3c2
jessicashong <jessicashong12@gmail.com>
2024-02-09 12:50:28 -0600

    valentine code

---
+++ be-mine/src/App.js
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import './App.css';
+
+const phrases = [
+  'No',
+  'Are you sure?',
+  'Try again',
+  'Beb, please',
+  "Don't do this to me",
+  'About to be bbij',
+  'u kidding me?',
+];
+
+
+function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPress, setYesPress] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  function handleNoClick() {
+    setNoCount(noCount + 1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
+
+  return (
+    <div className='valentine-container'>
+      {yesPress ? (
+        <>
+          <img alt='jump hug' src='https://media.tenor.com/A0p8Dwm8YYwAAAAi/akirambow-smile-person.gif' />
+          <div className='text'>Yay!!!!</div>
+          </>
+      ) : (
+
+        <>
+          <img 
+            alt='checking phone'
+            src='https://media1.tenor.com/m/YXhp8VXoWe0AAAAC/checking-couple.gif'
+          />
+
+          <div>Will you be my valentine?</div>
+          <div>
+            <button 
+              className='yesButton' 
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPress(true)}
+            >
+                Yes
+              </button>
+            <button
+              onClick={handleNoClick} className='noButton'
+            >
+              {getNoButtonText()}
+            </button>
+          </div>
+
+        </>
+        )}
+    </div>
+  )
+}
+
+export default App;
